e1488a209a327fb61ca63425e6f4ad6d4d6e9b4e
Dark-knight21 <ralphtalion@gmail.com>
2024-04-19 22:23:20 +0800

    Create AddProduct.vue
    Add product

---
+++ components/AddProduct.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <h2>Add Product</h2>
+      <form @submit.prevent="addNewProduct">
+        <input type="text" v-model="newProduct.name" placeholder="Name" required><br>
+        <input type="text" v-model="newProduct.description" placeholder="Description" required><br>
+        <input type="number" v-model="newProduct.price" placeholder="Price" required><br>
+        <button type="submit">Add Product</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AddProduct',
+    data() {
+      return {
+        newProduct: {
+          name: '',
+          description: '',
+          price: 0,
+        },
+      };
+    },
+    methods: {
+      addNewProduct() {
+        this.$store.dispatch('addProduct', this.newProduct);
+        // Reset newProduct data
+        this.newProduct = {
+          name: '',
+          description: '',
+          price: 0,
+        };
+        // Redirect to ProductList page after adding
+        this.$router.push('/');
+      },
+    },
+  };
+  </script>
+  
